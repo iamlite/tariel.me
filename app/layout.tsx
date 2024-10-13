0795e523a4a8c406e6475f0663570b60b58955dc
iamLite <hello@tariel.me>
2024-10-12 21:37:51 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,49 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Tariel Davidashvili',
-  description: '-'
+  description: '-',
+  authors: [{ name: 'Tariel Davidashvili', url: 'https://tarieldavids.com' }],
+  publisher: 'Tariel Davidashvili',
+  creator: 'Tariel Davidashvili',
+  generator: 'Next.js',
+  applicationName: 'Tariel Davidashvili',
+  metadataBase: new URL('https://tarieldavids.com'),
+  keywords: ['Tariel Davidashvili', 'Canada', 'Ottawa'],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  },
+  icons: {
+    icon: [
+      { url: '/assets/favicon.ico' },
+      { url: '/assets/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/assets/favicon.svg', type: 'image/svg+xml' }
+    ],
+    shortcut: '/assets/favicon.ico',
+    apple: '/assets/apple-touch-icon.png'
+  },
+  manifest: '/assets/site.webmanifest',
+  appleWebApp: {
+    title: 'Tariel Davidashvili',
+    statusBarStyle: 'black-translucent'
+  },
+  alternates: {
+    canonical: 'https://tarieldavids.com',
+    languages: {
+      'en-US': 'https://tarieldavids.com'
+    }
+  },
+  twitter: {
+    title: 'Tariel Davidashvili',
+    description: '-',
+    site: '@tarieldavids',
+    creator: 'Tariel Davidashvili',
+    images: 'https://tarieldavids.com/og.png'
+  }
 }
 
 export default function RootLayout({
